e22a0be3fee873a51c62658c9efb6498495cf885
sandeepgowda10 <49002009+sandeepgowda10@users.noreply.github.com>
2020-03-09 23:49:09 +0530

    Update settings

--- sandy/settings.py
+++ sandy/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qr$)bx@_7r$+g5(7@i7=%5zkhp&!9dvqtl)uvj8$2hq4u354n#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
